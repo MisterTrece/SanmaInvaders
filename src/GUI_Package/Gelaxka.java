1c6e84a6cb465c3f824d3e8b99469159b8d28920
Zabalaa325 <unaiizabalaa@gmail.com>
2026-03-05 20:59:16 +0100

    Update Gelaxka.java

--- src/GUI_Package/Gelaxka.java
+++ src/GUI_Package/Gelaxka.java
@@ -28,4 +28,27 @@ public class Gelaxka extends JLabel implements Observer {
    
     public int getXCoord() { return x; }
     public int getYCoord() { return y; }
-}
+    
+    public void eguneratu(Observable o, Object arg) {
+
+        if (arg instanceof Integer) {
+            int egoera = (Integer) arg;
+            
+            switch (egoera) {
+                case 0:
+                    this.setBackground(Color.BLACK); // Hutsik (Vacío)
+                    break;
+                case 1:
+                    this.setBackground(Color.GREEN); // Gurea (Nave aliada)
+                    break;
+                case 2:
+                    this.setBackground(Color.RED);   // Etsaia (Enemigo)
+                    break;
+                case 3:
+                    this.setBackground(Color.WHITE); // Tiroa (Disparo)
+                    break;
+            }
+        }
+    }
+}  
+            
