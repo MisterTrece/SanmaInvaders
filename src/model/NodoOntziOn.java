3a0499554d94611cd6b9bd0faaa5648ed27abbfe
Zabalaa325 <unaiizabalaa@gmail.com>
2026-04-03 02:59:26 +0200

    Update NodoOntziOn.java

--- src/model/NodoOntziOn.java
+++ src/model/NodoOntziOn.java
@@ -10,6 +10,10 @@ public class NodoOntziOn implements ElementuPixel{
 	protected int x;
 	protected int y;
 	
+	protected TiroEstrategia unekoa;
+    protected ArrayList<TiroEstrategia> motaGuztiak;
+    protected int armaIndizea = 0;
+	
 	public NodoOntziOn() {
 		this.x=55;
 		this.y=50;
@@ -21,6 +25,40 @@ public class NodoOntziOn implements ElementuPixel{
 		pixelak.add(JokalariFabrika.getJokalariFabrika().createOntziOna(1, 51, 55));
 		
 	}
+    motaGuztiak = new ArrayList<>();
+    motaGuztiak.add(new TiroPixelEstrategia()); 
+    
+    switch(pMota) {
+        case 1: 
+            motaGuztiak.add(new TiroErronboEstrategia());
+            break;
+        case 2: 
+            motaGuztiak.add(new TiroGeziEstrategia());
+            break;
+        case 3: 
+            motaGuztiak.add(new TiroGeziEstrategia());
+            motaGuztiak.add(new TiroErronboEstrategia());
+            break;
+    }
+
+    public void aldatuTiroMota() {
+        armaIndizea++;
+        if (armaIndizea >= motaGuztiak.size()) {
+            armaIndizea = 0;
+        }
+        unekoa = motaGuztiak.get(armaIndizea);
+    }
+    public ArrayList<Tiro> disparatu() {
+        if (unekoa.baduMuniziorik()) {
+            unekoa.munizioaGastatu();
+            // Disparamos usando la coordenada central del NODO, no la de un píxel suelto
+            return unekoa.tiroEgin(this.x, this.y); 
+        }
+        return new ArrayList<>(); 
+    
+    // El arma por defecto al empezar siempre será el Píxel (índice 0)
+    unekoa = motaGuztiak.get(0);
+}
 	
 	public int getX() {
 		return this.x;
